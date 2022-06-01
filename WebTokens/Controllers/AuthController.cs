fcb607ab423188fb47a681919a5a199426faa96b
Rachel Hoffman <tailsnick5@gmail.com>
2022-05-31 18:48:19 -0600

    Add project files.

---
+++ WebTokens/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebTokens.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        { }
+    }
+}
